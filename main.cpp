f31f9178b5be447db5e7014132d882be1d64d2af
shaon <shaon20aust@gmail.com>
2022-04-11 23:45:23 +0200

    Menu

--- main.cpp
+++ main.cpp
@@ -19,13 +19,13 @@ public:
 void student::calculate(){
     average = (eng_marks+math_marks+sci_marks+lang2_marks+cs_marks)/5.0;
     if (average>=90)
-        grade = "A";
+        grade = 'A';
     else if (average>=75)
-        grade = "B";
+        grade = 'B';
     else if (average>=50)
-        grade = "C";
+        grade = 'C';
     else
-    grade = "F";
+    grade = 'F';
 }
 
 void student::getdata(){
@@ -64,8 +64,62 @@ int student::retrollno() const{
     return rollno;
 }
 
+void create_student();
+void dipslay_sp(int);
+void display_all();
+void delete_student(int);
+void change_student(int);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    char ch;
+    //cout<<setprecision(2);
+    do{
+        char ch;
+        int num;
+        system("cls");
+        cout<<"\n\n\n\tMENU";
+        cout<<"\n\n\t1.Create Student Record";
+        cout<<"\n\n\t2.Search Student Record";
+        cout<<"\n\n\t3.Display All Student Record";
+        cout<<"\n\n\t4.Delete Student Record";
+        cout<<"\n\n\t5.Modify Student Record";
+        cout<<"\n\n\t6.Exit";
+        cout<<"\n\nWhat is your choice (1/2/3/4/5/6)?";
+        cin>>ch;
+        system("cls");
+        switch(ch){
+            case '1': create_student();
+            break;
+            case '2': cout<<"\n\n\tEnter the student roll number: ";
+            cin>>num;
+            dipslay_sp(num);break;
+            case '3': display_all();break;
+            case '4': cout<<"\n\n\tEnter the roll number: ";
+            cin>>num;
+            delete_student(num);break;
+            case '5': cout<<"\n\n\tEnter the roll number: ";
+            cin>>num;
+            change_student(num);break;
+            case '6': cout<<"Exiting, Thank You !!!";
+            exit(0);
+        }
+
+    }
+    while(ch!='6');
     return 0;
+}
+void create_student(){
+}
+
+void display_all(){
+}
+
+void dipslay_sp(int n){
+}
+
+void delete_student(int n){
+}
+
+void change_student(int n){
 }
